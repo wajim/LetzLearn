381cce374acd1deddc3a861089d393a7ae09f9dd
Wajim <luxjim3763@gmail.com>
2019-10-12 04:16:02 +0200

    Initial commit

---
+++ app/src/main/java/com/luxjim/letzlearn/ApiKeys.java
@@ -0,0 +1,9 @@
+package com.luxjim.letzlearn;
+
+/**
+ * Created by Jimmy on 31/03/2017.
+ */
+
+public class ApiKeys {
+    public static final String YANDEX_API_KEY = "trnsl.1.1.20170330T025248Z.f6ec0099f40cb3fe.ca45b82df1a1f9ad2a993ffc907887dbd43328d7";
+}
---
+++ app/src/main/java/com/luxjim/letzlearn/MainActivity.java
@@ -0,0 +1,127 @@
+package com.luxjim.letzlearn;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import com.luxjim.letzlearn.language.Language;
+import com.luxjim.letzlearn.translate.Translate;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edt1;
+    TextView txt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //admob
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+
+            }
+        });
+        //ads
+        AdView mBannerAd = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("D8628BC63271598CBE67943279248FFB")
+                .build();
+        mBannerAd.loadAd(adRequest);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final TextView txt1 = findViewById(R.id.textview3);
+        final EditText edt1 = findViewById(R.id.edtext1);
+        edt1.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        edt1.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        edt1.requestFocus();
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+
+                    Translate.setKey(ApiKeys.YANDEX_API_KEY);
+                    String term = edt1.getText().toString();
+                    String translatedText = Translate.execute(term, Language.FRENCH, Language.LUXEMBOURGISH);
+                    txt1.setText(translatedText);
+                } catch (Exception e) {
+                    // TODO: handle exception
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        Log.d("WAJIM******************",  "Ok for test");
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onResume() {
+
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+
+        super.onDestroy();
+    }
+}
