53788a8d4d3565e9ece1c7732c49e46d0693bde8
Wajim <luxjim3763@gmail.com>
2020-01-08 05:05:37 +0100

    Update MainActivity.java

--- app/src/main/java/com/luxjim/letzlearn/MainActivity.java
+++ app/src/main/java/com/luxjim/letzlearn/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dico);
+        setContentView(R.layout.activity_main);
 
         //admob
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
